78ee7c8a1b91a279fe2cf071b345469c53001c37
Pilfer <pilfer@live.com>
2014-01-15 17:57:48 -0500

    Added functions for uploading to Amazon S3

--- vine.php
+++ vine.php
@@ -1,16 +1,36 @@
 <?php
+
+//include Amazon S3 library bucket for upload
+//Found here but modified slightly to return the versionId in the response headers: https://github.com/tpyo/amazon-s3-php-class
+require_once("S3.php");
+
 class Vine{
 	
 	public $endpoint = "https://api.vineapp.com/";
-	public $useragent = "com.vine.iphone/1.1 (unknown, iPhone OS 6.1.3, iPad, Scale/1.000000)";
+	public $useragent = "iphone/106 (iPhone; iOS 7.0.4; Scale/2.00)";
+	
+	//for bucket uploads
+	public $s3_key = "AKIAJL2SSORTZ5AK6D4A";
+	public $s3_secret = "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";
+	
+	public $s3_bucket = "vines";
+	public $s3_url = "http://vines.s3.amazonaws.com/";
 	
 	//POST headers
 	public $post_headers = array(
-				"Host: api.vineapp.com",
-				"Proxy-Connection: keep-alive",
 				"Accept: */*",
-				"Accept-Language: nb, en, fr, de, ja, nl, it, es, pt, pt-PT, da, fi, sv, ko, zh-Hans, zh-Hant, ru, pl, tr, uk, ar, hr, cs, el, he, ro, sk, th, id, ms, en-GB, ca, hu, vi, en-us;q=0.8",
+				"Accept-Language: en;q=1, fr;q=0.9, de;q=0.8, zh-Hans;q=0.7, zh-Hant;q=0.6, ja;q=0.5",
 				"Connection: keep-alive",
+				"X-Vine-Client: ios/1.4.7",
+				"Content-Type: application/json"
+			);
+			
+	public $upload_headers = array(
+				"Accept: */*",
+				"Accept-Language: en;q=1, fr;q=0.9, de;q=0.8, zh-Hans;q=0.7, zh-Hant;q=0.6, ja;q=0.5",
+				"Connection: keep-alive",
+				"X-Vine-Client: ios/1.4.7",
+				"Content-Type: application/json"
 			);
 	
 	//GET headers
@@ -18,7 +38,7 @@ class Vine{
 				"Host: api.vineapp.com",
 				"Proxy-Connection: keep-alive",
 				"Accept: */*",
-				"Accept-Language: nb, en, fr, de, ja, nl, it, es, pt, pt-PT, da, fi, sv, ko, zh-Hans, zh-Hant, ru, pl, tr, uk, ar, hr, cs, el, he, ro, sk, th, id, ms, en-GB, ca, hu, vi, en-us;q=0.8",
+				"Accept-Language: en;q=1, fr;q=0.9, de;q=0.8, zh-Hans;q=0.7, zh-Hant;q=0.6, ja;q=0.5",
 				"Connection: keep-alive",
 			);
 	
@@ -29,11 +49,18 @@ class Vine{
 	public $email;
 	public $password;
 	
+	
 	//Just a string that the GET/POST functions use if you decide to enable a proxy. ip:port is the format you'll use.
 	public $proxy;
 	
 	public function __construct(){
 		//echo "Initialized Vine Class";
+	}
+	
+	
+	//Generate a v4 UUID - should be used when uploading to Amazon S3
+	public function genuuid() {
+		return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0x0fff ) | 0x4000, mt_rand( 0, 0x3fff ) | 0x8000, mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ));
 	}
 
 	//Make a POST request
@@ -47,6 +74,7 @@ class Vine{
 		}else{
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $this->post_headers);
 		}
+
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
@@ -110,6 +138,9 @@ class Vine{
 				break;
 			case "POST":
 				$response = $this->post($this->endpoint . $path,$data);
+				
+				var_dump($response);
+				
 				if($response != false){
 					$json_response = json_decode($response);
 					if($json_response){
@@ -130,6 +161,9 @@ class Vine{
 			"password" => $password
 		);
 		$login_response = $this->api("users/authenticate","POST",$data);
+		
+		//var_dump($login_response);
+		
 		if($login_response != false){
 			if($login_response->success == true){
 				$this->email = $email;
@@ -217,7 +251,7 @@ class Vine{
 	
 	//Searches username
 	public function searchUser($username,$page){
-		$search_response = $this->api("users/search/" . urlencode($username) . "?size=100&page=" . trim($page),"GET");
+		$search_response = $this->api("users/search/" . urlencode(trim($username)) . "?size=100&page=" . trim($page),"GET");
 		if($search_response != false){
 			if($search_response->success == true){
 				return $search_response;
@@ -271,6 +305,79 @@ class Vine{
 			return false;
 		}
 	}
+	
+	//uploads an avatar to amazon s3
+	//full path to file, name of file on s3 .jpg
+	//200x201
+	public function uploadAvatar($file,$remote_filename){
+		$s3 = new S3($this->s3_key,$this->s3_secret);
+		$k = $s3->putObjectFile($file, $this->s3_bucket, "avatars/" . $remote_filename);
+		if($k != false){
+			$version_id = $k;
+			$url = $this->s3_url . "avatars/" . $remote_filename . "?versionId=" . $version_id;
+			return $url;
+		}else{
+			return false;
+		}
+	}
+	
+	//uploads thumbnail to amazon s3
+	//full path to file, name of file on s3 + .mp4.jpg
+	//480x480
+	public function uploadThumbnail($file,$remote_filename){
+		$s3 = new S3($this->s3_key,$this->s3_secret);
+		$k = $s3->putObjectFile($file, $this->s3_bucket, "thumbs/" . $remote_filename);
+		if($k != false){
+			$version_id = $k;
+			$url = $this->s3_url . "thumbs/" . $remote_filename . "?versionId=" . $version_id;
+			return $url;
+		}else{
+			return false;
+		}
+	}
+	
+	//uploads video to amazon s3
+	//full path to file, name of file on s3 + .mp4
+	//480x480
+	public function uploadVideo($file,$remote_filename){
+		$s3 = new S3($this->s3_key,$this->s3_secret);
+		$k = $s3->putObjectFile($file, $this->s3_bucket, "videos/" . $remote_filename);
+		if($k != false){
+			$version_id = $k;
+			$url = $this->s3_url . "videos/" . $remote_filename . "?versionId=" . $version_id;
+			return $url;
+		}else{
+			return false;
+		}
+	}
+	
+	
+	//configure the vine - parameters below
+	/*
+	$params = array(
+		"thumbnailUrl" => $this->uploadThumnail(..),
+		"videoUrl" => $this->uploadVideo(..),
+		"description" => "Caption Here!",
+		"entities" => "[]"
+	);
+	
+	NOTE: It must be properly formatted and indented JSON when you post it.
+	Use JSON_PRETTY_PRINT or the equivelent function if you don't have PHP 5.5
+	Like so: $params = json_encode($params,JSON_PRETTY_PRINT);
+	*/
+	public function configureVine($params){
+		$config_response = $this->api("posts","POST", $params);
+		if($config_response != false){
+			if($config_response->success == true){
+				return $config_response;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}
+	
 	
 }//End of Class
 ?>
